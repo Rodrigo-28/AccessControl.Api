d2ed6ef25d09a3a39f18287b4c4b2e30287eeaa4
Rodrigo Romano <rodrigoromano2019@gmail.com>
2025-12-30 10:29:39 -0300

    added migrationInitial

---
+++ AccessControlApi.Infrastructure/Migrations/20251230132837_initialTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccessControlApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "user_id",
+                keyValue: 1,
+                column: "password",
+                value: "$2a$11$E3TFrOyXaIpbgH0IhjUyAuVombZaZt.vOyIkN2s/tdk6NpZZzrL6q");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "user_id",
+                keyValue: 1,
+                column: "password",
+                value: "$2a$11$zAB6P8IU5GPiDS8KsuQQD.JXp6HzYgiA633NMFhKseBcmRggubZoS");
+        }
+    }
+}
